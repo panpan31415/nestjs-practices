f8ce13aefc6d92df415c1a1a8c5f3417cd3ca35d
panpan <panpan31415@gmail.com>
2024-01-12 17:47:36 +0100

    extract NavLink component

--- foodies-starting-project/components/Header/index.js
+++ foodies-starting-project/components/Header/index.js
@@ -4,23 +4,25 @@ import classes from "./index.module.css";
 import Image from "next/image";
 import HeaderBackground from "./Background";
 import { Fragment } from "react";
+import NavLink from "./NavLink";
 export default function Header() {
   return (
     <Fragment>
       <HeaderBackground />
       <header className={classes.header}>
         <Link href="/" className={classes.logo}>
-          {/* priority: tells NextJs to load the image ASAP, do not use lay loading method on this image  */}
+          {/* priority: tells NextJs to load the image ASAP, do not use
+           lay loading method on this image  */}
           <Image src={logoImg} alt="A plate of food" priority />
           Next Level Food
         </Link>
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href={"/meals"}>Browse Meals</Link>
+              <NavLink href={"/meals"}> Browse meals </NavLink>
             </li>
             <li>
-              <Link href={"/community"}>Foodies Community</Link>
+              <NavLink href={"/community"}> Foodies Community </NavLink>
             </li>
           </ul>
         </nav>
