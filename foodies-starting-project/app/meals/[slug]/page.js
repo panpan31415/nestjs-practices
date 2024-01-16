3f8e91fd1276bdb3f7de48e21119b8473d75b662
panpan <panpan31415@gmail.com>
2024-01-16 17:54:54 +0100

    add page details page

---
+++ foodies-starting-project/app/meals/[slug]/page.js
@@ -0,0 +1,41 @@
+import { Fragment } from "react";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meal";
+import { notFound } from "next/navigation";
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by
+            <a href={`mailto:${meal.creator_email}`} type="email">
+              {meal.creator}
+            </a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </Fragment>
+  );
+}
