8f1da618a78979e0982ccf9d993238be5fec1630
panpan <panpan31415@gmail.com>
2024-01-17 16:33:04 +0100

    add a multi image selector

--- foodies-starting-project/components/meals/image-picker.js
+++ foodies-starting-project/components/meals/image-picker.js
@@ -2,6 +2,7 @@
 import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
 import Image from "next/image";
+import { MdDelete } from "react-icons/md";
 
 export default function ImagePicker({ label, name }) {
   const [pickedImages, setPickedImages] = useState([]);
@@ -21,8 +22,14 @@ export default function ImagePicker({ label, name }) {
     }
   };
 
-  const onClick = () => {
+  const onSelectImage = () => {
     imageInputRef.current.click();
+  };
+
+  const onRemoveImage = (index) => {
+    setPickedImages((pickedImages) => {
+      return pickedImages.filter((file, fileIndex) => fileIndex !== index);
+    });
   };
 
   return (
@@ -34,6 +41,13 @@ export default function ImagePicker({ label, name }) {
             pickedImages.map((file, index) => (
               <div className={classes.preview} key={index}>
                 <Image src={file} fill alt={"user selected file: " + index} />
+                <button
+                  type="button"
+                  onClick={() => onRemoveImage(index)}
+                  className={classes.remove}
+                >
+                  <MdDelete size={40} />
+                </button>
               </div>
             ))
           ) : (
@@ -54,7 +68,11 @@ export default function ImagePicker({ label, name }) {
           onChange={onImageChange}
         />
         {/* if type is submit, it will cause the form to send an post request */}
-        <button type="button" className={classes.button} onClick={onClick}>
+        <button
+          type="button"
+          className={classes.button}
+          onClick={onSelectImage}
+        >
           select an image
         </button>
       </div>
