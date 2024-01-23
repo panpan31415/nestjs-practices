46d24ee0e55f8167c38174833cad434d8717d549
panpan <panpan31415@gmail.com>
2024-01-23 13:51:04 +0100

    Add AWS S3 integration for image storage

--- foodies-starting-project/lib/meal.js
+++ foodies-starting-project/lib/meal.js
@@ -1,7 +1,11 @@
+import { S3 } from "@aws-sdk/client-s3";
 import sql from "better-sqlite3";
 import slugify from "slugify";
 import xss from "xss";
 import fs from "node:fs";
+const s3 = new S3({
+  region: "us-east-1",
+});
 const db = sql("meals.db");
 
 export async function getMeals() {
@@ -14,28 +18,21 @@ export function getMeal(slug) {
 }
 
 export async function saveMeal(meal) {
-  meal.slug = slugify(meal.title, {
-    lower: true, // specify all characters to be lowercase.
-  });
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
 
-  meal.instructions = xss(meal.instructions); // sanitize all the instructions that typed by user
+  const extension = meal.image.name.split(".").pop();
+  const fileName = `${meal.slug}.${extension}`;
 
-  // store image to public folder
-  console.log(meal.image);
-  const fileExtension = meal.image.name.split(".").pop();
-  const fileName = `${meal.slug}.${fileExtension}`;
-  const stream = fs.createWriteStream(`public/images/${fileName}`);
   const bufferedImage = await meal.image.arrayBuffer();
-  stream.write(Buffer.from(bufferedImage), (error) => {
-    if (error) {
-      throw new Error("Saving image failed!");
-    }
+
+  s3.putObject({
+    Bucket: process.env.AWS_BUCKET_FOLDER,
+    Key: fileName,
+    Body: Buffer.from(bufferedImage),
+    ContentType: meal.image.type,
   });
-  // after saving file to public folder
-  // we update image with image path.
-  // by default all the request will land on public
-  // folder, so we don't need to add "public" to the path
-  meal.image = `/images/${fileName}`;
+  meal.image = fileName;
 
   db.prepare(
     `
