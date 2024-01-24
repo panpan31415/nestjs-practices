f97cead80019ec5f6de64d13a4484987ed0b9aca
panpan <panpan31415@gmail.com>
2024-01-24 19:05:42 +0100

    Add page level meta data

--- foodies-starting-project/app/meals/page.js
+++ foodies-starting-project/app/meals/page.js
@@ -3,6 +3,7 @@ import classes from "./page.module.css";
 import Link from "next/link";
 import Meals from "@/components/meals";
 
+// page level meta data
 export const metadata = {
   title: "All Meals",
   description: "Browse the delicious meals shared by our vibrant community",
